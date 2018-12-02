043ccf5affd604b199b8ca943a0329610af87634
Jamie Burke <jjaburke@hotmail.com>
2018-12-02 17:29:51 +0000

    meta content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,10 +6,31 @@ import Footer from '../components/footer';
 
 import '../../sass/style.scss';
 
+const WEBSITE_DESCRIPTION = ""; // TODO
+const WEBSITE_TITLE = "Figure Figure";
+
+const DEFAULT_IMAGE_URL = ""; // TODO
+const DEFAULT_URL = "http://figurefigure.fr/";
+
 const TemplateWrapper = ({ children }) => (
     <div className="page-container">
         <link href="https://fonts.googleapis.com/css?family=Orbitron:400,700|Raleway" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet" />
+
+        <link rel="canonical" href={DEFAULT_URL} />
+
+        <meta name="description" content={WEBSITE_DESCRIPTION} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="figurefigure_fr" />
+        <meta name="twitter:image" content={DEFAULT_IMAGE_URL} />
+
+        <meta property="og:description" content={WEBSITE_DESCRIPTION} />
+        <meta property="og:site_name" content="Figure Figure" />
+        <meta property="og:title" content={WEBSITE_TITLE} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={DEFAULT_URL} />
+        <meta property="og:image" content={DEFAULT_IMAGE_URL} />
+        <meta property="og:image:alt" content={WEBSITE_TITLE} />
 
         <Helmet
             title="Figure Figure"
