bf6612e51157a5675c8a37e689c79ec6e868d487
Jamie Burke <jjaburke@hotmail.com>
2018-12-04 20:45:37 +0000

    Updated footer icons and padding.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,8 +5,8 @@ import Instagram from '../icon/instagram.icon';
 import Twitter from '../icon/twitter.icon';
 import './_footer.scss';
 
-const ICON_HEIGHT = 20;
-const ICON_WIDTH = 20;
+const ICON_HEIGHT = 36;
+const ICON_WIDTH = 36;
 const ICON_COLOR = 'black';
 
 const Footer = () => (
