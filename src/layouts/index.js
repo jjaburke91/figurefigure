c14c1e389cfbd81e8b648786a4e2082c388a27e9
Jamie Burke <jjaburke@hotmail.com>
2018-12-04 23:01:19 +0000

    correct layout to use helmet, as previous headers weren't in page <head> el.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import Helmet from 'react-helmet';
 
 import '../../sass/style.scss';
 
@@ -13,31 +14,33 @@ const DEFAULT_URL = "https://figurefigure.fr/";
 
 const TemplateWrapper = ({ children }) => (
     <div className="page-container">
-        <link href="https://fonts.googleapis.com/css?family=Orbitron:400,900|Raleway" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet" />
+        <Helmet>
+            <link href="https://fonts.googleapis.com/css?family=Orbitron:400,900|Raleway" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet" />
 
-        <link rel="canonical" href={DEFAULT_URL} />
+            <link rel="canonical" href={DEFAULT_URL} />
 
-        <meta name="description" content={WEBSITE_DESCRIPTION} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="figurefigure_fr" />
-        <meta name="twitter:image" content={DEFAULT_IMAGE_URL} />
+            <meta name="description" content={WEBSITE_DESCRIPTION} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="figurefigure_fr" />
+            <meta name="twitter:image" content={DEFAULT_IMAGE_URL} />
 
-        <meta property="og:description" content={WEBSITE_DESCRIPTION} />
-        <meta property="og:site_name" content="Figure Figure" />
-        <meta property="og:title" content={WEBSITE_TITLE} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={DEFAULT_URL} />
-        <meta property="og:image" content={DEFAULT_IMAGE_URL} />
-        <meta property="og:image:alt" content={WEBSITE_TITLE} />
+            <meta property="og:description" content={WEBSITE_DESCRIPTION} />
+            <meta property="og:site_name" content="Figure Figure" />
+            <meta property="og:title" content={WEBSITE_TITLE} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={DEFAULT_URL} />
+            <meta property="og:image" content={DEFAULT_IMAGE_URL} />
+            <meta property="og:image:alt" content={WEBSITE_TITLE} />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+        </Helmet>
 
         <Header/>
 
