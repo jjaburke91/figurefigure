8a26f459bcf74312ace0871c5f7d626cad6892c0
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-05-09 11:55:32 +0200

    cleanup

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,13 +9,13 @@ const ICON_HEIGHT = 26;
 const ICON_WIDTH = 26;
 const ICON_COLOR = 'black';
 
-const Footer = ({issue}) => {
-
-    return (<div className="row"><footer className="footer mt-auto py-3">
-        
-            <div className="bandef"><a href={issue.path}>NEW! PDF N°{issue.number} — {issue.title.toUpperCase()}</a></div>
-        
-    </footer></div>)
-};
+const Footer = ({issue}) => (
+    <div className="row">
+        <footer className="footer mt-auto py-3">
+            <div className="bandef">
+                <a href={issue.path}>NEW! PDF N°{issue.number} — {issue.title.toUpperCase()}</a>
+            </div>
+        </footer>
+    </div>);
 
 export default Footer;
--- src/components/issue/index.jsx
+++ src/components/issue/index.jsx
@@ -7,17 +7,8 @@ import './issue.scss';
 const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
 const Issue = ({issue}) => {
-
     const d = new Date(issue.date_of_issue);
     const formatedDate = monthNames[d.getUTCMonth()] + " " + d.getFullYear();
-
-    /*const fullPageClass = isFullPage ? "issue--full-height" : "";
-    let bg = isFullPage && issue.bg_href
-        ? <div className="cover" style={{backgroundImage: `linear-gradient(0, rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url(${withPrefix(issue.bg_href)})`}} />
-        : "" ;*/
-
-        //                <img className="issue__img" src={withPrefix(issue.image_href)} alt={`Issue ${issue.title} link`}/>
-
     return (
         <article className={`issue`}>
             <a href={withPrefix(issue.path)} target="_blank">
@@ -30,7 +21,6 @@ const Issue = ({issue}) => {
 };
 
 Issue.propTypes = {
-    //isFullPage: PropTypes.bool,
     issue: PropTypes.object.isRequired,
 };
 
