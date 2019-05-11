84d00cc59a6a7ab443b84ba6d711c46f4b4fd45b
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2019-05-11 18:37:22 +0200

    add pdf display

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -39,10 +39,21 @@ class EventsPage extends React.Component {
         <div ref={this.imgList} className="col-6 vh-site event-img-grid">
           {this.events.map((event, i) => (
             <div ref={event.ref} key={`event img ${i}`}>
-              <img className="event-img" src={withPrefix(event.img)} alt={event.title} />
-              <div className="event-descr">
-                <span className="event-date">{showMonthDate(event.date)}</span>
-                <span className="event-title">{event.title}</span>
+              {event.pdf
+                ? (
+                  <object width="100%" height="500" type="application/pdf" data={`${withPrefix(event.pdf)}?#zoom=95&scrollbar=0&toolbar=0&navpanes=0`}>
+                    <p>Insert your error message here, if the PDF cannot be displayed.</p>
+                  </object>
+                )
+                : <img className="event-img" src={withPrefix(event.img)} alt={event.title} />
+              }
+              <div className="event-descr row">
+                <span className="event-date col">{showMonthDate(event.date)}</span>
+                <span className="event-title col">
+                  {event.title}
+                  <br />
+                  {event.subtitle}
+                </span>
               </div>
             </div>
           ))}
@@ -53,7 +64,11 @@ class EventsPage extends React.Component {
               <li key={`event item ${i}`} onMouseEnter={this.showEvent.bind(this, event)}>
                 <span className="event-descr">
                   <span className="event-date">{showDayDate(event.date)}</span>
-                  <span className="event-title">{event.title}</span>
+                  <span className="event-title">
+                    {event.title}
+                    <br />
+                    {event.subtitle}
+                  </span>
                 </span>
               </li>
             ))}
@@ -73,8 +88,9 @@ export const pageQuery = graphql`
         node {
           events {
             title
+            subtitle
             date
-            img
+            pdf
           }
         }
       }
